31b4b4142fb6c04965549d08c7b1211a32de1692
Helen <redkina2508@gmail.com>
2023-07-08 10:04:41 +0300

    added tasks

--- js/script.js
+++ js/script.js
@@ -1,68 +1,68 @@
-// let userAge = prompt("How old are you?", "");
-// if (userAge >= 0 && userAge <= 11) {
-//    alert("You are a child");
-// } else if (userAge >= 12 && userAge <= 17) {
-//    alert("You are teenaiger");
-// } else if (userAge >= 18 && userAge <= 59) {
-//    alert("You are adult");
-// } else if (userAge >= 60) {
-//    alert("You are retired");
-// } else alert("Enter correct age");
+let userAge = prompt("How old are you?", "");
+if (userAge >= 0 && userAge <= 11) {
+   alert("You are a child");
+} else if (userAge >= 12 && userAge <= 17) {
+   alert("You are teenaiger");
+} else if (userAge >= 18 && userAge <= 59) {
+   alert("You are adult");
+} else if (userAge >= 60) {
+   alert("You are retired");
+} else alert("Enter correct age");
 
-// //--------task 2-------//
-// let enterNumber = prompt("Enter the number from 0 to 9");
-// switch (enterNumber) {
-//    case "0":
-//       alert(")");
-//       break;
-//    case "1":
-//       alert("!");
-//       break;
-//    case "2":
-//       alert("@");
-//       break;
-//    case "3":
-//       alert("#");
-//       break;
-//    case "4":
-//       alert("$");
-//       break;
-//    case "5":
-//       alert("%");
-//       break;
-//    case "6":
-//       alert("^");
-//       break;
-//    case "7":
-//       alert("&");
-//       break;
-//    case "8":
-//       alert("*");
-//       break;
-//    case "9":
-//       alert("(");
-//       break;
-//    default:
-//       alert("Enter correct number");
-// }
+//--------task 2-------//
+let enterNumber = prompt("Enter the number from 0 to 9");
+switch (enterNumber) {
+   case "0":
+      alert(")");
+      break;
+   case "1":
+      alert("!");
+      break;
+   case "2":
+      alert("@");
+      break;
+   case "3":
+      alert("#");
+      break;
+   case "4":
+      alert("$");
+      break;
+   case "5":
+      alert("%");
+      break;
+   case "6":
+      alert("^");
+      break;
+   case "7":
+      alert("&");
+      break;
+   case "8":
+      alert("*");
+      break;
+   case "9":
+      alert("(");
+      break;
+   default:
+      alert("Enter correct number");
+}
 //------------task3-------------//
-// let numberFirst = +prompt("Enter first number", "");
-// let numberSecond = +prompt("Enter second number", "");
-// let sumNumbersFirst = (numberFirst * (numberFirst - 1)) / 2;
-// let sumNumbersSecond = (numberSecond * (numberSecond + 1)) / 2;
-// let sumNumbers = sumNumbersSecond - sumNumbersFirst;
-// alert(sumNumbers);
+let numberFirst = +prompt("Enter first number", "");
+let numberSecond = +prompt("Enter second number", "");
+let sumNumbersFirst = (numberFirst * (numberFirst - 1)) / 2;
+let sumNumbersSecond = (numberSecond * (numberSecond + 1)) / 2;
+let sumNumbers = sumNumbersSecond - sumNumbersFirst;
+alert(sumNumbers);
 
-// let number_1 = +prompt("Enter first number", "");
-// let number_2 = +prompt("Enter second number", "");
+let number_1 = +prompt("Enter first number", "");
+let number_2 = +prompt("Enter second number", "");
 
-// let sum = 0;
-// do {
-//    sum += number_1;
-//    number_1 += 1;
-// } while (number_1 <= number_2);
+let sum = 0;
+do {
+   sum += number_1;
+   number_1 += 1;
+} while (number_1 <= number_2);
 
-// alert(sum);
+alert(sum);
 
 //--------palindrom --------------//
 let enterNumberCheckPalindrom = prompt("Enter the number");
