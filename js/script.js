a1207decf823d3d0d1680f2bd6a38bd2c77c141f
Helen <redkina2508@gmail.com>
2023-07-09 23:40:00 +0300

    added tasks

--- js/script.js
+++ js/script.js
@@ -1,87 +1,64 @@
-let userAge = prompt("How old are you?", "");
-if (userAge >= 0 && userAge <= 11) {
-   alert("You are a child");
-} else if (userAge >= 12 && userAge <= 17) {
-   alert("You are teenaiger");
-} else if (userAge >= 18 && userAge <= 59) {
-   alert("You are adult");
-} else if (userAge >= 60) {
-   alert("You are retired");
-} else alert("Enter correct age");
+"use sstrict";
+//---------task 1---------//
 
-//--------task 2-------//
-let enterNumber = prompt("Enter the number from 0 to 9");
-switch (enterNumber) {
-   case "0":
-      alert(")");
-      break;
-   case "1":
-      alert("!");
-      break;
-   case "2":
-      alert("@");
-      break;
-   case "3":
-      alert("#");
-      break;
-   case "4":
-      alert("$");
-      break;
-   case "5":
-      alert("%");
-      break;
-   case "6":
-      alert("^");
-      break;
-   case "7":
-      alert("&");
-      break;
-   case "8":
-      alert("*");
-      break;
-   case "9":
-      alert("(");
-      break;
-   default:
-      alert("Enter correct number");
-}
-//------------task3-------------//
-let numberFirst = +prompt("Enter first number", "");
-let numberSecond = +prompt("Enter second number", "");
-if (numberFirst > numberSecond) {
-   [numberFirst, numberSecond] = [numberSecond, numberFirst];
-}
-let sumNumbersFirst = (numberFirst * (numberFirst - 1)) / 2;
-let sumNumbersSecond = (numberSecond * (numberSecond + 1)) / 2;
-let sumNumbers = sumNumbersSecond - sumNumbersFirst;
-alert(sumNumbers);
+let btn = document.querySelectorAll(".button");
+let answer = document.querySelectorAll(".answer");
 
-let number_1 = +prompt("Enter first number", "");
-let number_2 = +prompt("Enter second number", "");
-if (number_1 > number_2) {
-   [number_1, number_2] = [number_2, number_1];
-}
-let sum = 0;
-do {
-   sum += number_1;
-   number_1 += 1;
-} while (number_1 <= number_2);
+btn[0].addEventListener("click", outputAnswer);
 
-alert(sum);
-
-//--------palindrom --------------//
-let enterNumberCheckPalindrom = prompt("Enter the number check palindrom");
-if (
-   enterNumberCheckPalindrom.split("").reverse().join("") ===
-   enterNumberCheckPalindrom
-) {
-   alert("Yes it is a palindrome");
-} else alert("No, it's not a palindrome");
-
-//------------multiplication tables ---------//
-
-for (let i = 1; i < 11; i++) {
-   for (let j = 1; j < 11; j++) {
-      console.log(`${i} * ${j} = ${i * j}`);
+function outputAnswer() {
+   let kindOfFunction = [
+      "Function Declaration",
+      "Function Expression",
+      "Arrow Function ",
+   ];
+   for (let i = 0; i < kindOfFunction.length; i++) {
+      answer[0].innerHTML += `<p>${i + 1}. ${kindOfFunction[i]}</p> `;
    }
+   // answer[0].style.display === "none"
+   //    ? (answer[0].style.display = "block")
+   //    : (answer[0].style.display = "none");
 }
+
+btn[1].addEventListener("click", numberOfArgumen);
+
+function numberOfArgumen() {
+   answer[1].innerHTML = "<img src='../assets/img/task_2.png'>";
+}
+
+btn[2].addEventListener("click", compareNumber);
+
+function compareNumber() {
+   answer[2].innerHTML = "<img src='../assets/img/task_3.png'>";
+}
+
+//------task 2-------------//
+
+function countArguments(...args) {
+   console.log(args.length);
+}
+
+countArguments(1, 2, 7, 8);
+
+//---------task 3------------//
+
+function compareTwoNubers(x, y) {
+   if (x < y) {
+      return -1;
+   } else if (x > y) {
+      return 1;
+   } else return 0;
+}
+console.log(compareTwoNubers(7, 7));
+
+//----------task 4----------//
+
+function factorial(n) {
+   if (n === 0) {
+      return 1;
+   } else return n * factorial(n - 1);
+}
+
+console.log(factorial(3));
+
+//---------task 5----------//
